5820bd60f674dcd4e820f0989e0343bc66b59bbb
Favisya <vikulin@sfedu.ru>
2022-06-17 17:11:01 +0300

    commit

--- app/models/bouquet.rb
+++ app/models/bouquet.rb
@@ -1,5 +1,5 @@
 class Bouquet < ApplicationRecord
-  validates :number,numericality: true
+  #validates :number,numericality: true
   # validates :number,length: {minimum: 11, maximum: 12}
   #has_and_belongs_to_many :flowers
   has_many :bouquets_flowers_joins
