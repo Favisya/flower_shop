d0060a8133ec4067bdd96b8d9fdb6ae848bfc64b
Dmitriy <79097372+Favisya@users.noreply.github.com>
2022-06-28 01:50:35 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   resources :users, only: [:new, :create, :destroy, :edit, :update, :index]
   resources :sessions, only: [:new, :create, :index]
   resources :bouquets
@@ -12,13 +12,11 @@ Rails.application.routes.draw do
   get '/plus/:id', action: :plus, controller: 'bouquets'
   get '/minus/:id', action: :minus, controller: 'bouquets'
   get '/save/:id', action: :save, controller: 'bouquets'
-  # get '/add/:id', action: :add_in_bouquet, controller: 'bouquets'
   get '/add/:id', to: 'bouquets#add_in_bouquet'
   get 'sold', action: :sold, controller: 'bouquets'
   get 'removeall', action: :remove_all, controller: 'bouquets'
   get '/remove/:id', action: :remove, controller: 'bouquets'
   get 'profile', to: 'users#show'
   get 'admin', to: 'users#admin'
-  get 'flowerApi', to: 'flowers#flowerApi'
-  
+
 end
